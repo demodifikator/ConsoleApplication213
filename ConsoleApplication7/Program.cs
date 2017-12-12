dedda865a7256b65d8bedf90f9726f839521a26c
Lenovo <demodifikator@gmail.com>
2017-12-13 00:24:12 +0200

    Числа Фибоначчи

--- ConsoleApplication7/Program.cs
+++ ConsoleApplication7/Program.cs
@@ -10,6 +10,15 @@ namespace ConsoleApplication7
     {
         static void Main(string[] args)
         {
+            ulong[] fib = new ulong[100];
+            byte i;
+
+            fib[0] = 0; fib[1] = 1; fib[2] = 1;
+            for (i = 3; i < 100; i++)
+                fib[i] = fib[i - 1] + fib[i - 2];
+
+            for (i = 0; i < 100; i++)
+                Console.WriteLine($"{i + 1}) {fib[i]}");
         }
     }
 }
